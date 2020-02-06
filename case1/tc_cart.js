74d253d9d5d8ec7aca0666cf566ddb71b0f1ba63
joseph brown <jbrown562@west-mec.org>
2020-02-05 18:23:01 -0700

    fixed code

---
+++ case1/tc_cart.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 1
+
+   Author: joseph brown
+   Date: 2/4/20  
+   
+   Filename: tc_cart.js
+	
+*/
+
+var orderTotal = 0;
+var cartHTML = "<table>";
+cartHTML += "<tr>";
+cartHTML += "<th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th>";
+cartHTML += "</tr>";
+
+for(var i = 0; i < item.length; i++){
+   cartHTML += "<tr>";
+   cartHTML += "<td><img src='tc_" + item[i] + ".png' alt='item'/></td>";
+   cartHTML += "<td>" + itemDescription[i] + "</td>";
+   cartHTML += "<td>$" + itemPrice[i] + "</td>";
+   cartHTML += "<td>" + itemQty[i] + "</td>";
+   var itemCost = itemPrice[i] * itemQty[i];
+   cartHTML += "<td>$" + itemCost + "</td></tr>";
+   orderTotal += itemCost;
+}
+
+cartHTML += "<tr>";
+cartHTML += "<td colspan='4'>Subtotal</td>";
+cartHTML += "<td>$" + orderTotal + "</td>";
+cartHTML += "</tr>";
+cartHTML += "</table>";
+
+document.getElementById("cart").innerHTML = cartHTML;
--- case1/tc_order.js
+++ case1/tc_order.js
@@ -17,3 +17,7 @@
    
 */
 
+var item = ("10582","23015","41807","10041");
+var itemDescription = ("1975 Green Bay Packers Football (signed),item 10582","Tom Landry 1955 Football Card (unsigned), item 23015","1916 Army-Navy Game, Framed Photo (signed), item 41807","Protective Card Sheets, item 10041");
+var itemPrice = ("149.93","89.98","334.93","22.67");
+var itemQty = ("1","1","1","4");
